18f1ee59c720d4e0e1131155113010dc932a43ab
Tyler-Applegate <tyler.applegate@gmail.com>
2021-06-17 16:28:52 -0500

    updated functions

--- wrangle_mall.py
+++ wrangle_mall.py
@@ -65,10 +65,14 @@ def summarize(df):
     for col in df.columns:
         if col in cat_cols:
             print(df[col].value_counts())
+            print('--------------------------------------------')
+            print('')
         else:
             print(df[col].value_counts(bins=10, sort=False))
+            print('--------------------------------------------')
+            print('')
     print('----------------------------------------------------')
-    print('Nulls in DataFrae by Column: ')
+    print('Nulls in DataFrame by Column: ')
     print(nulls_by_col(df))
     print('----------------------------------------------------')
     print('Nulls in DataFrame by Rows: ')
@@ -86,15 +90,50 @@ def get_upper_outliers(s, k=1.5):
 
 def add_upper_outlier_columns(df, k=1.5):
     for col in df.select_dtypes('number'):
-        df[col + '_outliers_upper'] = get_upper_outliers(df[col], k)
+        df[col + '_upper_outliers'] = get_upper_outliers(df[col], k)
+    return df
+
+def get_lower_outliers(s, k=1.5):
+    q1, q3 = s.quantile([0.25, 0.75])
+    iqr = q3 - q1
+    lower_bound = q1 - k * iqr
+    return s.apply(lambda x:max([x - lower_bound, 0]))
+
+def add_lower_outlier_columns(df, k=1.5):
+    for col in df.select_dtypes('number'):
+        df[col + '_lower_outliers'] = get_lower_outliers(df[col], k)
     return df
 
 def outlier_describe(df):
-    outlier_cols = [col for col in df.columns if col.endswith('_outliers_upper')]
+    outlier_cols = [col for col in df.columns if col.endswith('_outliers')]
     for col in outlier_cols:
         print(col, ': ')
         subset = df[col][df[col] > 0]
         print(subset.describe())
+
+def drop_outliers(df, col_list, k=1.5):
+    '''
+    This function takes in a dataframe and removes outliers that are k * the IQR
+    '''
+    
+    for col in col_list:
+
+        q_25, q_75 = df[col].quantile([0.25, 0.75])
+        q_iqr = q_75 - q_25
+        q_upper = q_75 + (k * q_iqr)
+        q_lower = q_25 - (k * q_iqr)
+        df = df[df[col] > q_lower]
+        df = df[df[col] < q_upper]
+#       these bottome two lines are only necessary if previous functions have been callled
+#       if this function is run BEFORE add_upper/lower, then these columns need to be commented out
+        outlier_cols = [col for col in df.columns if col.endswith('_outliers')]
+        df = df.drop(columns=outlier_cols)
+        
+    return df     
+
+def mall_encoder(df, col):
+    df = pd.get_dummies(df, columns=col, drop_first=True)
+    return df
         
 def split_data(df):
     '''
@@ -106,3 +145,67 @@ def split_data(df):
                                        test_size=.3, 
                                        random_state=1221)
     return train, validate, test
+
+def min_max_scale(train, validate, test, numeric_cols):
+    """
+    this function takes in 3 dataframes with the same columns,
+    a list of numeric column names (because the scaler can only work with numeric columns),
+    and fits a min-max scaler to the first dataframe and transforms all
+    3 dataframes using that scaler.
+    it returns 3 dataframes with the same column names and scaled values.
+    """
+    # create the scaler object and fit it to X_train (i.e. identify min and max)
+    # if copy = false, inplace row normalization happens and avoids a copy (if the input is already a numpy array).
+
+    scaler = MinMaxScaler(copy=True).fit(train[numeric_cols])
+
+    # scale X_train, X_validate, X_test using the mins and maxes stored in the scaler derived from X_train.
+    #
+    train_scaled_array = scaler.transform(train[numeric_cols])
+    validate_scaled_array = scaler.transform(validate[numeric_cols])
+    test_scaled_array = scaler.transform(test[numeric_cols])
+
+    # convert arrays to dataframes
+    train_scaled = pd.DataFrame(train_scaled_array, columns=numeric_cols).set_index(
+        [train.index.values]
+    )
+
+    validate_scaled = pd.DataFrame(
+        validate_scaled_array, columns=numeric_cols
+    ).set_index([validate.index.values])
+
+    test_scaled = pd.DataFrame(test_scaled_array, columns=numeric_cols).set_index(
+        [test.index.values]
+    )
+
+    return train_scaled, validate_scaled, test_scaled
+
+def wrangle_mall_data():
+    col_list = ['age', 'annual_income', 'spending_score']
+    # let's acquire our data...
+    df = get_mallcustomer_data()
+    # summarize the data
+    print(summarize(df))
+    # add upper outlier columns
+    df = add_upper_outlier_columns(df)
+    # add lower outlier columns
+    df = add_lower_outlier_columns(df)
+    # describe the outliers
+    print(outlier_describe(df))
+    # drop outliers
+    df = drop_outliers(df, col_list)
+    # split the data
+    train, validate, test = split_data(df)
+    # drop missing values from train
+    train = train.dropna()
+    # scale the data
+    train_scaled, \
+    validate_scaled, \
+    test_scaled = min_max_scale(train, validate, test, col_list)
+    print(f'          train shape: {train.shape}')
+    print(f'       validate shape: {validate.shape}')
+    print(f'           test shape: {test.shape}')
+    print(f'   train_scaled shape: {train_scaled.shape}')
+    print(f'validate_scaled shape: {validate_scaled.shape}')
+    print(f'    test_scaled shape: {test_scaled.shape}')
+    
